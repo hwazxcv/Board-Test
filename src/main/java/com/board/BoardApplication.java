f5d74c0d141c1e64d22a5cb7e0f0e128e308e252
hwazxcv <hwazxcv@naver.com>
2023-11-09 17:53:28 +0900

    초기 설정중

--- src/main/java/com/board/BoardApplication.java
+++ src/main/java/com/board/BoardApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoardApplication {
 
 
-//안녕하세요
+
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
---
+++ src/main/java/com/board/configs/Dbconfig.java
@@ -0,0 +1,11 @@
+package com.board.configs;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Dbconfig {
+
+
+
+
+}
